e9569fb470e27a2bf464c526bf072b3dc1ec7571
FtM <fmassei@gmail.com>
2013-12-21 16:23:49 -0800

    - multithreaded version of sgpperformace added

--- test/sgpperformance/main.c
+++ test/sgpperformance/main.c
@@ -19,10 +19,12 @@
 #include <string.h>
 #include <libamspace.h>
 #include <sgp4.h>
+#include <sys/time.h>
+#include <pthread.h>
 
-#include <time.h>
+static gravconst_st g;
 
-double elapsedTime(void) { // returns 0 seconds first time called
+static double elapsedTime(void) { // returns 0 seconds first time called
     static struct timeval t0;
     struct timeval tv;
     gettimeofday(&tv, 0);
@@ -30,27 +32,9 @@ double elapsedTime(void) { // returns 0 seconds first time called
         t0 = tv;
     return tv.tv_sec - t0.tv_sec + (tv.tv_usec - t0.tv_usec) / 1000000.;
 }
-int main(const int argc, const char *argv[])
+static int test_nonThreaded(tle_st *tle)
 {
-    tle_st *tle, *p;
-    FILE *in;
-    double et;
-    gravconst_st g;
-    if (argc!=2) {
-        fprintf(stderr, "Usage: sgpperformance <TLEfile>\n");
-        return EXIT_FAILURE;
-    }
-    if ((in = fopen(argv[1], "rb"))==NULL) {
-        perror("fopen");
-        return EXIT_FAILURE;
-    }
-    if ((tle = tle_read(in, 1, 0))==NULL) {
-        fprintf(stderr, "Error reading TLEs\n");
-        return EXIT_FAILURE;
-    }
-    fclose(in);
-    getgravconst(WGS84, &g);
-    et = elapsedTime();
+    tle_st *p;
     for (p=tle; p!=NULL; p=p->next) {
         sat_st sat;
         satFromTLE(&sat, p);
@@ -68,8 +52,69 @@ int main(const int argc, const char *argv[])
             sat.t += 10;
         }
     }
+    return 0;
+}
+
+static void *threadP(tle_st *p)
+{
+    sat_st sat;
+    satFromTLE(&sat, p);
+    sgp4_init(&sat, &g);
+    sat.t = 0;
+    while (sat.t<=1440) {
+        if (sgp4(&sat, &g)!=0) {
+            fprintf(stderr, "Error %d on %d\n", p->satNo, sat.error);
+            break;
+        }
+        /*printf("%d %16.8f %16.8f %16.8f %16.8f %12.9f %12.9f %12.9f\n",
+            p->satNo, sat.t,
+                      sat.pos[0], sat.pos[1], sat.pos[2],
+                      sat.vel[0], sat.vel[1], sat.vel[2]);*/
+        sat.t += 10;
+    }
+    return NULL;
+}
+static int test_threaded(tle_st *tle)
+{
+    tle_st *p;
+    pthread_t th[15000];
+    int i=0;
+    for (p=tle; p!=NULL; p=p->next, ++i) {
+        pthread_create(&th[i], NULL, threadP, p);
+    }
+    for (; i>=0; --i)
+        pthread_join(th[i], NULL);
+    return 0;
+}
+int main(const int argc, const char *argv[])
+{
+    tle_st *tle;
+    FILE *in;
+    double et;
+    if (argc!=2) {
+        fprintf(stderr, "Usage: sgpperformance <TLEfile>\n");
+        return EXIT_FAILURE;
+    }
+    if ((in = fopen(argv[1], "rb"))==NULL) {
+        perror("fopen");
+        return EXIT_FAILURE;
+    }
+    if ((tle = tle_read(in, 1, 0))==NULL) {
+        fprintf(stderr, "Error reading TLEs\n");
+        return EXIT_FAILURE;
+    }
+    fclose(in);
+    getgravconst(WGS84, &g);
+    et = elapsedTime();
+    test_nonThreaded(tle);
     et = elapsedTime()-et;
     fprintf(stderr, "elapsed time: %f\n", et);
+
+    et = elapsedTime();
+    test_threaded(tle);
+    et = elapsedTime()-et;
+    fprintf(stderr, "elapsed time: %f\n", et);
+
     tle_destroy(&tle);
     return 0;
 }
